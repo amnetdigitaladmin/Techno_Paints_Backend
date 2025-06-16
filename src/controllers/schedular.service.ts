81c6e675c594452db348f19f9ef51da29f975403
raju-amnet <raju.orugallu@amnetdigital.com>
2025-06-16 11:14:19 +0530

    added

--- src/controllers/schedular.service.ts
+++ src/controllers/schedular.service.ts
@@ -4,20 +4,20 @@ import AMCService from '../controllers/AMC.service';
 
 class schedularService {
     public async runSchedulars() {
-        // cron.schedule(process.env.AMC_INACTIVE_CRON_TIME, async () => {  // Everyday 11:00 AM IST
-        //     console.log('Make AMC inactive Based on AMC End Date Schedular Started', moment().format("DD-MM-YYYY HH:mm:ss"));
-        //     await AMCService.inactiveAMCsAfterSubscriptionEnded();
-        //     console.log('Make AMC inactive Based on AMC End Date Schedular Completed', moment().format("DD-MM-YYYY HH:mm:ss"));
-        // }, {
-        //     timezone: 'Asia/Kolkata'
-        // }),
-        // cron.schedule(process.env.CARRY_FORWARD_CRON_TIME, async () => {  // Every start of the year first day
-        //     console.log('Carry Forward Schedular Started', moment().format("DD-MM-YYYY HH:mm:ss"));
-        //     await AMCService.carryForwardSchedular()
-        //     console.log('Carry ForwardSchedular Schedular Completed', moment().format("DD-MM-YYYY HH:mm:ss"));
-        // }, {
-        //     timezone: 'Asia/Kolkata'
-        // })
+        cron.schedule('0 11 * * *', async () => {  // Everyday 11:00 AM IST
+            console.log('Make AMC inactive Based on AMC End Date Schedular Started', moment().format("DD-MM-YYYY HH:mm:ss"));
+            await AMCService.inactiveAMCsAfterSubscriptionEnded();
+            console.log('Make AMC inactive Based on AMC End Date Schedular Completed', moment().format("DD-MM-YYYY HH:mm:ss"));
+        }, {
+            timezone: 'Asia/Kolkata'
+        }),
+        cron.schedule('0 12 * * *', async () => {  // Every start of the year first day
+            console.log('Carry Forward Schedular Started', moment().format("DD-MM-YYYY HH:mm:ss"));
+            await AMCService.carryForwardSchedular()
+            console.log('Carry ForwardSchedular Schedular Completed', moment().format("DD-MM-YYYY HH:mm:ss"));
+        }, {
+            timezone: 'Asia/Kolkata'
+        })
     }
 
     
